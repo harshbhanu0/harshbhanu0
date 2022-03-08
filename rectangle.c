99db26719f6a0644ebf66d88db72503a0dbe5144
harshbhanu0 <101184600+harshbhanu0@users.noreply.github.com>
2022-03-08 16:56:34 +0530

    Create rectangle.c

---
+++ rectangle.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	double a=10.0;
+	double b=20.0;
+	double per=0.0;
+	double area=0.0;
+	
+	per = 2*(a+b);
+	area = a*b;
+	
+	printf("Perimeter is : %f\n",per);
+	printf("Area is : %f\n",area);
+	
+	return 0;
+	
+}
